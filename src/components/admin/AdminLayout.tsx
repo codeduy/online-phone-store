68a7d81143c950f66bdf3e6255ba576604055348
Nguyễn Trần Duy <52100787@student.tdtu.edu.vn>
2025-01-12 09:59:35 +0700

    update adminpage - sidebar

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -1,18 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
-import Sidebar from "./Sidebar";
+import AdminSidebar from "./AdminSidebar";
+import { Sidebar } from 'primereact/sidebar';
 import '/src/styles/tailwind.css';
+import { useState } from 'react';
+import { Button } from 'primereact/button';
 
 const AdminLayout = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handleSidebarItemClick = () => {
+    setVisible(false);
+  };
+
   return (
     <div className="h-screen flex flex-col">
       <header className="bg-white shadow-md border border-gray-300 rounded-lg m-4 p-4"> {/* Added border and rounded-lg */}
         <Header />
+        <Button icon="pi pi-bars" onClick={() => setVisible(!visible)} 
+        className="p-button-text border mt-3 p-2 hover:bg-green-500 cursor-pointer" />
       </header>
-      <div className="flex flex-1">
-        <aside className="w-60 bg-white shadow-md border border-gray-300 rounded-lg">
-          <Sidebar />
-        </aside>
+      <Sidebar visible={visible} onHide={() => setVisible(false)} className="w-50">
+        <AdminSidebar onItemClick={handleSidebarItemClick} />
+      </Sidebar>
+      <div className="flex flex-1">        
         <main className="flex-1 p-4 overflow-auto bg-gray-100 border border-gray-300 rounded-lg ml-4 mr-4">
           <Outlet />
         </main>
--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,59 +1,59 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'primereact/button';
 
-const Sidebar = () => {
+const AdminSidebar = ({ onItemClick }: { onItemClick: () => void }) => {
   const navigate = useNavigate();
   
   const menuItems = [
     {
       label: 'Trang chủ',
       icon: 'pi pi-home',
-      command: () => navigate('/admin')
+      command: () => {navigate('/admin');onItemClick()}
     },
     {
       label: 'Quản lý sản phẩm',
       icon: 'pi pi-box',
-      command: () => navigate('/admin/products')
+      command: () => {navigate('/admin/products');onItemClick();}
     },
     {
       label: 'Quản lý đơn hàng',
       icon: 'pi pi-shopping-cart',
-      command: () => navigate('/admin/orders')
+      command: () => {navigate('/admin/orders');onItemClick();}
     },
     {
       label: 'Quản lý khách hàng', 
       icon: 'pi pi-users',
-      command: () => navigate('/admin/customers')
+      command: () => {navigate('/admin/customers');onItemClick();}
     },
     {
       label: 'Quản lý thanh toán',
       icon: 'pi pi-credit-card',
-      command: () => navigate('/admin/payments')
+      command: () => {navigate('/admin/payments');onItemClick();}
     },
     {
       label: 'Quản lý chi nhánh',
       icon: 'pi pi-building',
-      command: () => navigate('/admin/branches')
+      command: () => {navigate('/admin/branches');onItemClick();}
     },
     {
       label: 'Quản lý nhân viên',
       icon: 'pi pi-user',
-      command: () => navigate('/admin/staff')
+      command: () => {navigate('/admin/staff');onItemClick();}
     },
     {
       label: 'Quản lý cấp quản lý',
       icon: 'pi pi-users',
-      command: () => navigate('/admin/managers')
+      command: () => {navigate('/admin/managers');onItemClick();}
     },
     {
       label: 'Tin tức & Sự kiện',
       icon: 'pi pi-bell',
-      command: () => navigate('/admin/news')
+      command: () => {navigate('/admin/news');onItemClick();}
     },
     {
       label: 'Báo cáo thống kê',
       icon: 'pi pi-chart-bar', 
-      command: () => navigate('/admin/statistics')
+      command: () => {navigate('/admin/statistics');onItemClick();}
     }
   ];
 
@@ -64,7 +64,7 @@ const Sidebar = () => {
           key={index}
           label={item.label}
           icon={item.icon}
-          className="p-2 w-full mb-2 p-button-text text-left cursor-pointer border border-gray-300 rounded hover:bg-gray-200 hover:border-blue-500"
+          className="block p-2 w-full mb-2 p-button-text text-left cursor-pointer border border-gray-300 rounded hover:bg-gray-200 hover:border-blue-500"
           onClick={item.command}
         />
       ))}
@@ -72,4 +72,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar;
+export default AdminSidebar;
