897d7c2353097622b9fcceb1c59a5cbc82d56acb
Nguyễn Trần Duy <52100787@student.tdtu.edu.vn>
2025-02-06 20:22:22 +0700

    first commit

---
+++ routes/categories.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Category = require("../models/categoryModel");
+
+const router = express.Router();
+
+// Lấy tất cả sản phẩm
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.find(); // Truy vấn tất cả sản phẩm
+
+    if (categories.length === 0) {
+      return res.status(404).json({ message: 'No categories found' });
+    }
+    res.json(categories); // Trả về dữ liệu
+  } catch (err) {
+    console.log("Error retrieving products:", err); // Log lỗi nếu có
+    res.status(500).json({ message: 'Error retrieving products', error: err });
+  }
+});
+
+module.exports = router;
